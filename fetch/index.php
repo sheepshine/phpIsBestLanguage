66ba2e1c7f31f1c7f331b0ec1453e0d009eceeb3
yangchaoben <842293890@qq.com>
2016-11-19 10:44:56 +0800

    2016-11-19 10:44:55

---
+++ fetch/index.php
@@ -0,0 +1,32 @@
+<?php
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname="test";
+
+    $conn=new mysqli($servername,$username,$password,$dbname);
+
+    if(!$conn){
+        die("连接失败".mysqli_connect_error());
+    }else{
+        echo "连接成功";
+    }
+    //$query = mysqli_query($conn,"INSERT INTO mydata (name, title)VALUES ('hehe','222222')");
+    $query = mysqli_query($conn,"select * from mydata");
+    var_dump($query);
+    
+    // while($row=mysqli_fetch_row($query)){
+    //     print_r($row);
+    // }
+
+    // while($row=mysqli_fetch_array($query,MYSQL_ASSOC)){
+    //     print_r($row);
+    //     echo $row['name']."<br>";
+    // }
+
+    while($row=mysqli_fetch_aSSOC($query)){
+        print_r($row);
+        echo $row['name']."<br>";
+    }
+
+?>
